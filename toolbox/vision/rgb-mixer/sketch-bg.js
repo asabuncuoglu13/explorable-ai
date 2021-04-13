01b13c386d19a6610a5af5adac309d0a1f956f4e
alpay sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2021-04-13 11:51:56 +0300

    rgb-mixer background update

--- toolbox/vision/rgb-mixer/sketch-bg.js
+++ toolbox/vision/rgb-mixer/sketch-bg.js
@@ -1,30 +1,30 @@
+var canvasWidth, canvasHeight;
 var redSlider, greenSlider, blueSlider;
 var sliderR, sliderG, sliderB, textX, boxY, sliderX;
 var monitorX, monitorY, monitorSize;
 var circleDiameter;
-var monitorSVG;
+var bg;
 
-/*
+
 function preload() {
-    monitorSVG = loadSVG("./monitor.svg");
+    bg = loadImage("./bg.png");
 }
-*/
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    var canvasWidth = windowWidth;
-    var canvasHeight = windowHeight;
+    canvasWidth = windowWidth;
+    canvasHeight = windowHeight;
     sliderR = canvasHeight * 0.3;
     sliderG = canvasHeight * 0.4;
     sliderB = canvasHeight * 0.5;
-    monitorX = canvasWidth * 0.75;
-    monitorY = canvasHeight * 0.4;
-    monitorSize = Math.max(160, canvasWidth * 0.3, canvasHeight * 0.3);
+    monitorX = canvasWidth * 0.8;
+    monitorY = canvasHeight * 0.45;
+    monitorSize = Math.max(160, canvasWidth * 0.25, canvasHeight * 0.25);
     circleDiameter = monitorSize / 2;
     boxY = canvasHeight * 0.6;
-    sliderX = 25;
-    var sliderSize = canvasWidth * 0.45;
+    sliderX = canvasWidth * 0.1;
+    var sliderSize = canvasWidth * 0.4;
     textX = sliderX + sliderSize + 10;
 
     textSize(20);
@@ -46,9 +46,13 @@ function setup() {
 }
 
 function draw() {
-    background(225);
+    fill("#5281FF")
+    rect(0, 0, canvasWidth, canvasHeight / 2);
+
+    fill("#F2AE6F")
+    rect(0, canvasHeight / 2, canvasWidth, canvasHeight / 2);
     imageMode(CENTER);
-    //image(monitorSVG, monitorX, monitorY, monitorSize, monitorSize);
+    image(bg, canvasWidth / 2, canvasHeight * 0.44, canvasWidth, canvasWidth * 0.5625);
     push();
 
     // Get values from sliders
@@ -58,11 +62,11 @@ function draw() {
 
     push();
 
-    fill("#0003a1");
+    fill("#6e70eb");
     text("B", textX, sliderB);
-    fill("#00a12b");
+    fill("#91d9a3");
     text("G", textX, sliderG);
-    fill("#a12600");
+    fill("#d97575");
     text("R", textX, sliderR);
 
     // Create color boxes
@@ -78,7 +82,7 @@ function draw() {
     rect(120, 0, size, size);
     fill(r, g, b);
     rect(180, 0, size, size);
-    fill(0);
+    fill(255);
     text("+", 40, 22)
     text("+", 100, 22)
     text("=", 160, 22)
@@ -98,7 +102,6 @@ function draw() {
     noStroke();
     rectMode(CENTER);
     ellipseMode(CENTER);
-    rect(monitorX, monitorY, monitorSize, monitorSize);
     blendMode(ADD);
     translate(80, 105);
     var nColors = 3;
